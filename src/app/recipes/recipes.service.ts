2ea888684ef201718da8cb5020cd813f84070e7b
Anatole.Couasnon <Anatole.Couasnon@UC00115>
2020-02-07 18:51:11 +0100

    tuto impro ionic

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: '1',
+      title: 'Recipe 1',
+      imageURL: 'https://gpfr-glps.innogamescdn.com/media/images/artwork-grepo-battle-wallpaper.32aad9f2.jpg',
+      ingredients: ['Frites', 'Viande', 'Glace']
+    },
+    {
+      id: '2',
+      title: 'Recipe 2',
+      imageURL: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      ingredients: ['truc', 'much', 'bidule']
+    }
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+        return recipe.id === recipeId;
+      })};
+  }
+}
